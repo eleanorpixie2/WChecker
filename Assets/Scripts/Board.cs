00154589e3035cb0f0ab232308fbd160b2ab1368
tess.olson <tess.olson@T3600-CRUSHER>
2019-04-12 15:47:31 -0500

    Got board to spawn pieces and spaces

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public List<BoardSpace> board;
+    public GameObject[] board;
     //piece prefabs
-    public GameObject blackPiece;
-    public GameObject whitePiece;
+    public Piece blackPiece;
+    public Piece whitePiece;
     //space prefabs
     public BoardSpace whiteSpace;
     public BoardSpace blackSpace;
     //List of pieces
     public List<Piece> whitePieces;
     public List<Piece> blackPieces;
-
+    //space location in scene
     private Vector3 spaceLocation;
+    private float intialX;
     // Start is called before the first frame update
     void Start()
     {
-        board = new List<BoardSpace>();
         whitePieces = new List<Piece>();
         blackPieces = new List<Piece>();
         spaceLocation = whiteSpace.transform.position;
+        intialX = spaceLocation.x;
         PopulateBoard();
+        board = GameObject.FindGameObjectsWithTag("Space");
+
+        SpawnPieces();
     }
 
     // Update is called once per frame
@@ -32,22 +36,73 @@ public class Board : MonoBehaviour
         
     }
 
+    GameObject GetSpace(int x, int y)
+    {
+        //find correct object based on grid location
+        foreach(GameObject space in board)
+        {
+            if(space.GetComponent<BoardSpace>().location==new Vector2(x,y))
+            {
+                return space;
+            }
+            
+        }
+        return null;
+    }
 
     void SpawnPieces()
     {
+        //columns
         for (int y = 0; y < 8; y++)
         {
+            //rows
             for (int x = 0; x < 8; x++)
             {
-                if(y<3)
+                //first 3 are white
+                if (y < 3)
                 {
-
+                    GameObject space = GetSpace(x, y);
+                    space.transform.position = spaceLocation;
+                    //only spawn if the space is black
+                    if (space.GetComponent<BoardSpace>()._spaceColor == SpaceColor.black)
+                    {
+                        whitePiece.currentSpace = space.GetComponent<BoardSpace>();
+                        whitePiece._color = SpaceColor.white;
+                        whitePiece.isKing = false;
+                        whitePiece.gameObject.transform.position = spaceLocation;
+                        whitePieces.Add(whitePiece);
+                        Instantiate(whitePiece.gameObject);
+                    }
                 }
-                else if( y>=5)
+                //empty
+                else if (y >= 3 && y < 5)
                 {
-
+                    GameObject space = GetSpace(x, y);
+                    space.transform.position = spaceLocation;
                 }
+                //last 3 are black
+                else if (y >= 5)
+                {
+                    GameObject space = GetSpace(x, y);
+                    space.transform.position = spaceLocation;
+                    //only spawn if the space is black
+                    if (space.GetComponent<BoardSpace>()._spaceColor == SpaceColor.black)
+                    {
+                        blackPiece.currentSpace = space.GetComponent<BoardSpace>();
+                        blackPiece._color = SpaceColor.black;
+                        blackPiece.isKing = false;
+                        blackPiece.gameObject.transform.position = spaceLocation;
+                        blackPieces.Add(blackPiece);
+                        Instantiate(blackPiece.gameObject);
+                    }
+                }
+                //move to the right
+                spaceLocation.x += 1.42f;
             }
+            //reset x
+            spaceLocation.x = intialX;
+            //move up
+            spaceLocation.z +=1.42f;
         }
     }
 
@@ -67,13 +122,14 @@ public class Board : MonoBehaviour
                     if (j%2 == 0)
                     {
                         whiteSpace.SetInital(SpaceColor.white, new Vector2(j, i));
-                        board.Add(whiteSpace);
+                        Instantiate(whiteSpace.gameObject);
                     }
                     //odd column
                     else
                     {
                         blackSpace.SetInital(SpaceColor.black, new Vector2(j, i));
-                        board.Add(whiteSpace);
+                        Instantiate(blackSpace.gameObject);
+
                     }
                 }
                 //all odd rows
@@ -83,14 +139,15 @@ public class Board : MonoBehaviour
                     if (j % 2 == 0)
                     {
                         blackSpace.SetInital(SpaceColor.black, new Vector2(j, i));
-                        board.Add(whiteSpace);
+                        Instantiate(blackSpace.gameObject);
 
                     }
                     //odd column
                     else
                     {
                         whiteSpace.SetInital(SpaceColor.white, new Vector2(j, i));
-                        board.Add(whiteSpace);
+                        Instantiate(whiteSpace.gameObject);
+
                     }
                 }
             }
