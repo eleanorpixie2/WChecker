c0d6f1bad462b7a6c637e063a75b3f1abb2c4316
adan.benitez <adan.benitez@loop.colum.edu>
2019-04-12 14:06:01 -0500

    Adding More Assests, and Getting Click working

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -9,12 +9,20 @@ public class PieceManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        boardRef = GameObject.FindGameObjectWithTag("Board").GetComponent<Board>();
+        //boardRef = GameObject.FindGameObjectWithTag("Board").GetComponent<Board>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //boardRef.
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                print("Was clicked");
+            }
+        }
     }
 }
