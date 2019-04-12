55e55a6171e0de47a35f478a1f731059f12a17e4
tess.olson <tess.olson@T3600-CRUSHER>
2019-04-12 18:38:20 -0500

    Update ValidMove.cs

--- Assets/Scripts/ValidMove.cs
+++ Assets/Scripts/ValidMove.cs
@@ -19,32 +19,36 @@ public class ValidMove : MonoBehaviour
     public bool CheckValidMove(Piece pieceToMove,BoardSpace spaceToMove)
     {
         bool valid = false;
-        if (pieceToMove.currentSpace.location.x + 1 == spaceToMove.location.x
-            && pieceToMove.currentSpace.location.y == spaceToMove.location.y)
+        //upper left
+        if (pieceToMove.currentSpace.location.x - 1 == spaceToMove.location.x
+            && pieceToMove.currentSpace.location.y - 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+            {
+                valid = true;
+            }
+        }
+        //upper right
+        else if (pieceToMove.currentSpace.location.x - 1 == spaceToMove.location.x
+            && pieceToMove.currentSpace.location.y + 1 == spaceToMove.location.y)
         {
             if (spaceToMove.currentPiece == null)
             {
                 valid = true;
             }
         }
-        else if (pieceToMove.currentSpace.location.x -1 == spaceToMove.location.x 
-            && pieceToMove.currentSpace.location.y==spaceToMove.location.y)
-        {
-            if (spaceToMove.currentPiece == null)
-            {
-                valid = true;
-            }
-        }
-        else if (pieceToMove.currentSpace.location.x == spaceToMove.location.x
-        && pieceToMove.currentSpace.location.y-1 == spaceToMove.location.y)
-        {
-            if(pieceToMove.isKing && spaceToMove.currentPiece==null)
-            {
-                valid = true;
-            }
-        }
+        //lower left
         else if (pieceToMove.currentSpace.location.x + 1 == spaceToMove.location.x
-        && pieceToMove.currentSpace.location.y+1 == spaceToMove.location.y)
+        && pieceToMove.currentSpace.location.y - 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+            {
+                valid = true;
+            }
+        }
+        //lower right
+        else if (pieceToMove.currentSpace.location.x + 1 == spaceToMove.location.x
+        && pieceToMove.currentSpace.location.y + 1 == spaceToMove.location.y)
         {
             if (spaceToMove.currentPiece == null)
             {
