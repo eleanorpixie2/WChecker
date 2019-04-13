80b7cb4ca7acc491a73744c3c983d196b6eb3d84
tess.olson <tess.olson@T3600-CRUSHER>
2019-04-12 19:28:25 -0500

    got pieces moving correctly

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PieceManager : MonoBehaviour
 {
     Board boardRef;
-    GameObject selectedPiece;
+    public GameObject selectedPiece;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,17 +29,123 @@ public class PieceManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit) && selectedPiece == null)
             {
-                selectedPiece = hit.collider.gameObject;
+                if (hit.collider.gameObject.tag == "Player")
+                    selectedPiece = hit.collider.gameObject;
             }
             else if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.gameObject.GetComponent<BoardSpace>())
+                BoardSpace space = hit.collider.gameObject.GetComponent<BoardSpace>();
+                if (space)
                 {
-                    selectedPiece.transform.position = hit.collider.gameObject.GetComponent<BoxCollider>().transform.position;
+                    Piece _selected = selectedPiece.GetComponent<Piece>();
+                    if (CheckValidMove(_selected, space))
+                    {
+                        selectedPiece.transform.position = hit.collider.gameObject.GetComponent<BoxCollider>().transform.position;
+                        //get old space
+                        BoardSpace current = _selected.currentSpace;
+                        //set new space
+                        _selected.currentSpace = space;
+                        //set the current piece for the space
+                        space.SetCurrentPiece(selectedPiece);
+                        //reset the current piece for the old space
+                        current.SetCurrentPiece();
+                        selectedPiece = null;
+                    }
+                }
+                else
+                {
                     selectedPiece = null;
                 }
-                
+
+
+            }
+            else
+            {
+                selectedPiece = null;
             }
         }
+    }
+
+    public bool CheckValidMove(Piece pieceToMove, BoardSpace spaceToMove)
+    {
+        bool valid = false;
+        //upper left
+        if (pieceToMove.currentSpace.location.x - 1 == spaceToMove.location.x
+            && pieceToMove.currentSpace.location.y - 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove._color == SpaceColor.white)
+            {
+                if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+            else
+            {
+                if (spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+        }
+        //upper right
+        else if (pieceToMove.currentSpace.location.x - 1 == spaceToMove.location.x
+            && pieceToMove.currentSpace.location.y + 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove._color == SpaceColor.black)
+            {
+                if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+            else
+            {
+                if (spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+        }
+        //lower left
+        else if (pieceToMove.currentSpace.location.x + 1 == spaceToMove.location.x
+        && pieceToMove.currentSpace.location.y - 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove._color == SpaceColor.white)
+            {
+                if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+            else
+            {
+                if (spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+        }
+        //lower right
+        else if (pieceToMove.currentSpace.location.x + 1 == spaceToMove.location.x
+        && pieceToMove.currentSpace.location.y + 1 == spaceToMove.location.y)
+        {
+            if (pieceToMove._color == SpaceColor.black)
+            {
+                if (pieceToMove.isKing && spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+            else
+            {
+                if (spaceToMove.currentPiece == null)
+                {
+                    valid = true;
+                }
+            }
+        }
+
+        return valid;
     }
 }
