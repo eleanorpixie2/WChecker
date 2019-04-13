ba2eea45b40119882c0b928262fb5ee84de429d2
tess.olson <tess.olson@T3600-CRUSHER>
2019-04-13 13:21:52 -0500

    Added revive power up

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +10,11 @@ public class PowerUp : MonoBehaviour
     public PlayerManager playerManager;
     public Animator animWhite;
     public Animator animBlack;
-    public Button blackPowerUp;
-    public Button WhitePowerUp;
+    public Button blackPowerUpRevive;
+    public Button WhitePowerUpRevive;
+    public Board board;
+    public GameObject whitePiece;
+    public GameObject blackPiece;
     private int numberOfKillsWhitePlayer;
     private int numberOfKillsBlackPlayer;
     private int currentWhitePieces;
@@ -30,7 +35,74 @@ public class PowerUp : MonoBehaviour
         //intial amount of pieces on the board
         currentWhitePieces = playerManager.numberOfWhitePiecesOnBoard;
         currentBlackPieces = playerManager.numberOfBlackPiecesOnBoard;
+
+        Button btnBlack = blackPowerUpRevive.GetComponent<Button>();
+        btnBlack.onClick.AddListener(TaskOnClick2);
+
+        Button btnWhite = WhitePowerUpRevive.GetComponent<Button>();
+        btnWhite.onClick.AddListener(TaskOnClick);
+        print(whitePiece);
+
+
     }
+
+    private void TaskOnClick2()
+    {
+        if (playerManager.currentTurn == colorTurn.black && canChoosePowerUpBlackPlayer)
+        {
+            GameObject space = null;
+            Piece p = blackPiece.GetComponent<Piece>();
+            foreach (GameObject bs in GameObject.FindGameObjectsWithTag("Space"))
+            {
+                if (bs.GetComponent<BoardSpace>().location.y >= 5)
+                {
+                    if (bs.GetComponent<BoardSpace>()._spaceColor == SpaceColor.black && bs.GetComponent<BoardSpace>().currentPiece == null)
+                    {
+                        space = bs;
+                    }
+                }
+            }
+            p.currentSpace = space.GetComponent<BoardSpace>();
+            p._color = SpaceColor.black;
+            p.isKing = false;
+            blackPiece.gameObject.transform.position = space.transform.position;
+            GameObject piece = (GameObject)PrefabUtility.InstantiatePrefab(blackPiece);
+            PrefabUtility.UnpackPrefabInstance(piece, PrefabUnpackMode.Completely, InteractionMode.UserAction);
+            space.GetComponent<BoardSpace>().SetCurrentPiece(piece);
+            playerManager.numberOfBlackPiecesOnBoard++;
+            playerManager.UpdateText();
+        }
+    }
+
+    private void TaskOnClick()
+    {
+        if (playerManager.currentTurn == colorTurn.white && canChoosePowerUpWhitePlayer)
+        {
+            print(whitePiece);
+            GameObject space = null;
+            Piece p = whitePiece.GetComponent<Piece>();
+            foreach (GameObject bs in GameObject.FindGameObjectsWithTag("Space"))
+            {
+                if (bs.GetComponent<BoardSpace>().location.y < 3)
+                {
+                    if (bs.GetComponent<BoardSpace>()._spaceColor == SpaceColor.black && bs.GetComponent<BoardSpace>().currentPiece == null)
+                    {
+                        space = bs;
+                    }
+                }
+            }
+            p.currentSpace = space.GetComponent<BoardSpace>();
+            p._color = SpaceColor.white;
+            p.isKing = false;
+            whitePiece.gameObject.transform.position = space.transform.position;
+            GameObject piece = (GameObject)PrefabUtility.InstantiatePrefab(whitePiece);
+            PrefabUtility.UnpackPrefabInstance(piece, PrefabUnpackMode.Completely, InteractionMode.UserAction);
+            space.GetComponent<BoardSpace>().SetCurrentPiece(piece);
+            playerManager.numberOfWhitePiecesOnBoard++;
+            playerManager.UpdateText();
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -44,11 +116,11 @@ public class PowerUp : MonoBehaviour
                 //get how many pieces have been removed from the opponent
                 int numberOfKills = currentBlackPieces - playerManager.numberOfBlackPiecesOnBoard;
                 //increase the number of kills
-                numberOfKillsWhitePlayer += numberOfKills;
+                numberOfKillsBlackPlayer += numberOfKills;
                 //reset the current amount
                 currentBlackPieces = playerManager.numberOfBlackPiecesOnBoard;
-                animWhite.SetInteger("Kills", numberOfKillsWhitePlayer);
-                if(numberOfKills>=4)
+                animWhite.SetInteger("Kills", numberOfKillsBlackPlayer);
+                if(numberOfKillsBlackPlayer >= 4)
                 {
                     canChoosePowerUpWhitePlayer = true;
                     numberOfKills = 4;
@@ -63,13 +135,13 @@ public class PowerUp : MonoBehaviour
                 //get how many pieces have been removed from the opponent
                 int numberOfKills = currentWhitePieces - playerManager.numberOfWhitePiecesOnBoard;
                 //increase the number of kills
-                numberOfKillsBlackPlayer += numberOfKills;
+                numberOfKillsWhitePlayer += numberOfKills;
                 //reset the current amount
                 currentWhitePieces = playerManager.numberOfWhitePiecesOnBoard;
-                animBlack.SetInteger("Kills", numberOfKillsBlackPlayer);
-                if (numberOfKills >= 4)
+                animBlack.SetInteger("Kills", numberOfKillsWhitePlayer);
+                if (numberOfKillsWhitePlayer >= 4 && playerManager.numberOfWhitePiecesOnBoard < 12)
                 {
-                    canChoosePowerUpBlackPlayer = true;
+                    canChoosePowerUpWhitePlayer = true;
                     numberOfKills = 4;
                 }
             }
